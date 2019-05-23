e415b55323ad2692ea90bcf832e8584b264178a4
Qumra <39854812+Qumra@users.noreply.github.com>
2019-05-23 15:36:02 +0800

    Update Main.jsx

--- src/views/Main/Main.jsx
+++ src/views/Main/Main.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Header from './Header'
+import Bottom from './Bottom'
 import cssObj from './Main.css'
 class Main extends Component {
     render(){
         return <div className={cssObj.Main}>
         <Header></Header>
+            <Bottom></Bottom>
         </div>
     }
 }
