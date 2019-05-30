184b5f0e3ac2f72cc76f37677969fee969857acb
Qumra <39854812+Qumra@users.noreply.github.com>
2019-05-30 16:20:31 +0800

    Update Config.jsx

--- src/views/system/Config/Config.jsx
+++ src/views/system/Config/Config.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
+import { Row, Col} from 'antd';
+import LeftNav from './LeftNav'
 class Config extends Component {
-    render(){
-        return <div>配置</div>
+    render() {
+        return <div>
+            <Row>
+                <Col span={8}><LeftNav></LeftNav></Col>
+                <Col span={16}>.col-16</Col>
+            </Row>
+        </div>
     }
 }
 export default Config
