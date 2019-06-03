6eec6ea27b1bf36dca74eee8068f01b1e769d5e4
Qumra <39854812+Qumra@users.noreply.github.com>
2019-06-03 11:09:15 +0800

    Update Config.jsx

--- src/views/system/Config/Config.jsx
+++ src/views/system/Config/Config.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 import { Row, Col} from 'antd';
 import LeftNav from './LeftNav'
+import { Route} from 'react-router-dom';
+import Conference from './Conference/Conference'
+const match='';
 class Config extends Component {
+    constructor(props){
+        super(props)
+        const {match}  = props.match;
+    }
     render() {
         return <div>
             <Row>
-                <Col span={8}><LeftNav></LeftNav></Col>
-                <Col span={16}>.col-16</Col>
+                <Col span={4}><LeftNav></LeftNav></Col>
+                <Col span={20}>.col-16
+                <Route  path={`${match.url}/Conference`} component={Conference}></Route>
+                </Col>
             </Row>
         </div>
     }
