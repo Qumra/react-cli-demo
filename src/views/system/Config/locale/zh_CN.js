1b2758a8d6bd709f50d04dd499085bfd254743d7
Qumra <39854812+Qumra@users.noreply.github.com>
2019-06-27 19:24:57 +0800

    Update zh_CN.js

--- src/views/system/Config/locale/zh_CN.js
+++ src/views/system/Config/locale/zh_CN.js
@@ -1,5 +1,9 @@
 const zh_CN = {
     ConferenceConfig:"会议配置",
-    ConfAreaConfig:"分区策略配置"
+    ConfAreaConfig:"分区策略配置",
+      NlogV2Config:"NlogV2配置",
+    DeviceConfig:"设备配置",
+    EmailConfig:"Email和短信配置",
+    NetConfig:"网络服务配置"
 }
 export default zh_CN
