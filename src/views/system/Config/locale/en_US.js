8be99cd1dbbb70a45753ec09352e3c486e846346
Qumra <39854812+Qumra@users.noreply.github.com>
2019-06-27 19:24:34 +0800

    Update en_US.js

--- src/views/system/Config/locale/en_US.js
+++ src/views/system/Config/locale/en_US.js
@@ -1,5 +1,9 @@
 const en_US = {
     ConferenceConfig:"Conference",
-    ConfAreaConfig:"Range Setting Policy"
+    ConfAreaConfig:"Range Setting Policy",
+      NlogV2Config:"NlogV2Config",
+    DeviceConfig:"DeviceConfig",
+    EmailConfig:"EmailConfig",
+    NetConfig:"NetConfig"
 }
 export default en_US
