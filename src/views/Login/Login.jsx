7516ad852e42240514914d69bf6a3aec0704f349
Qumra <39854812+Qumra@users.noreply.github.com>
2019-07-04 18:24:26 +0800

    Update Login.jsx

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,34 +1,29 @@
 import React, { Component } from "react";
 import cssObj from "./Login.css";
-import LoginNav from "./LoginNav/LoginNav.jsx"
-import Logo from "@/components/Logo/Logo.jsx"
+import LoginNav from "./LoginNav/LoginNav"
+import Logo from "@/components/Logo/Logo"
 import LoginContain from "./LoginContain/LoginContain"
-import history from '@/config/history';
-import intl, { moment } from '@/config/i18n'
-import { Calendar } from 'antd';
-
 class Login extends Component {
     constructor() {
         super()
         this.state = {
-            //   logoStyle:cssObj.logoImg
+            locale: null,
         }
-    }
+    }   
     render() {
         return (
-            <div className={cssObj.Login}>
-                <LoginNav></LoginNav>
-                <div className={cssObj.bgDiv}>
-                    <Logo width="400px" height="90px"></Logo>
-                    <LoginContain></LoginContain>
-                    <button onClick={() => history.push({ pathname: '/main' })}>{intl.get('redirect')}</button>
+            
+                <div className={cssObj.Login}>
+                    <LoginNav></LoginNav>
+                    <div className={cssObj.bgDiv}>
+                        <Logo width='318px' height='81px'></Logo>
+                        <LoginContain></LoginContain>
+                    </div>
                 </div>
-                <div style={{ width: 319, border: '1px solid #d9d9d9', borderRadius: 4 }}>
-                    <Calendar fullscreen={false} value={moment()} />>
-                </div>
-            </div>
+           
         )
     }
 }
+
 
 export default Login
