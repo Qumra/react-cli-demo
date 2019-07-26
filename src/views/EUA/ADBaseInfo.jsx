aca63a031ea5ce689c9d612a43d326e73c795897
Qumra <39854812+Qumra@users.noreply.github.com>
2019-07-26 17:50:19 +0800

    Create ADBaseInfo.jsx

---
+++ src/views/EUA/ADBaseInfo.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Tabs, Form, Switch, Icon, Input, Tooltip, Button } from 'antd';
+import cssObj from './EUADetail.css';
+class ADBaseInfo extends Component {
+    render() {
+        return (
+            <div>111</div>
+        );
+    }
+}
+export default ADBaseInfo;
