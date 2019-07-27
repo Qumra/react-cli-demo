0c3ba9a5a26777c729a1edd29989189eb7642083
Qumra <39854812+Qumra@users.noreply.github.com>
2019-07-27 18:51:41 +0800

    Update EUADetailFirst.jsx

--- src/views/EUA/EUADetailFirst.jsx
+++ src/views/EUA/EUADetailFirst.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import {Icon } from 'antd';
+import {Icon, Tabs } from 'antd';
 import cssObj from './EUADetailFirst.css';
 import FirstBaseForm from './FirstBaseForm';
+const {TabPane} = Tabs;
 class EUADetailFirst extends Component {
     render() {
         
@@ -12,14 +13,21 @@ class EUADetailFirst extends Component {
                     <span>企业通讯录管理</span>
                     <Icon type="question-circle" theme="outlined" className={cssObj.questionIcon}/>
                 </div>
-                {/* <div className={cssObj.euaContentMid}>
-                    <div className={cssObj.unlink}>
+                <div className={cssObj.euaContentMid}>
+                    {/* <div className={cssObj.unlink}>
                         <img src="" alt="未对接图" className={cssObj.unlinkImg}/>
                         <div className={cssObj.linkButton}>对接地址本服务器</div>
                         <span>还未对接地址本服务器，请对接服务器</span>
+                    </div> */}
+                    <div className={cssObj.firstLink}>
+                        <Tabs defaultActiveKey="1" >
+                            <TabPane tab="基本信息" key="1">
+                                <FirstBaseForm></FirstBaseForm>
+                            </TabPane>
+                        </Tabs>
                     </div>
-                </div> */}
-                <FirstBaseForm></FirstBaseForm>
+                   
+                </div>
             </div>
         </div>);
     }
