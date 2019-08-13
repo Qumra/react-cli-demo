917bc3ca10c436671ffa7d4bca45b546401d0f5b
Qumra <39854812+Qumra@users.noreply.github.com>
2019-08-13 18:12:46 +0800

    Update and rename src/views/mcu/AddMcu.jsx to src/views/mcu/add/AddMcu.jsx

--- src/views/mcu/add/AddMcu.jsx
+++ src/views/mcu/add/AddMcu.jsx
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 import cssObj from './AddMcu.css';
 import { Icon, Button, Input, Form} from 'antd';
 import AddMCUInfo from './AddMCUInfo';
+import {zh_CN_Device} from '@/locale/zh_CN';
+import {en_US_Device} from '@/locale/en_US';
+import {setLocale} from '@/config/i18n';
+import { FormattedMessage, injectIntl } from 'react-intl';
 class AddMcu extends Component {
+    constructor() {
+        super();
+        setLocale('zh-CN', zh_CN_Device);
+        setLocale('en-US', en_US_Device);
+    }
+    goback=()=>{
+        this.props.history.goBack();
+    }
     render() {
         return(
             <div className={cssObj.addMcu}>
                 <div className={cssObj.mcuContent}>
                     <div className={cssObj.mcuContentTitle}>
-                        <Icon type="left-circle" theme="outlined" className={cssObj.leftIcon}/>
-                        <span>添加MCU</span>
+                        <Icon type="left-circle" theme="outlined" className={cssObj.leftIcon} onClick={this.goback}/>
+                        <span><FormattedMessage id="MCU_AddMCU" /></span>
                         <Icon type="question-circle" theme="outlined" className={cssObj.questionIcon}/>
                     </div>
                     <div className={cssObj.mcuContentMid}>
@@ -24,4 +36,4 @@ class AddMcu extends Component {
     }
 }
 
-export default AddMcu;
+export default injectIntl(AddMcu);
