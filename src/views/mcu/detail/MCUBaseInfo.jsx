9c0694a443d208a47ad96837459b4bf4b5b707bb
Qumra <39854812+Qumra@users.noreply.github.com>
2019-08-13 18:15:47 +0800

    Update and rename src/views/mcu/MCUBaseInfo.jsx to src/views/mcu/detail/MCUBaseInfo.jsx

--- src/views/mcu/detail/MCUBaseInfo.jsx
+++ src/views/mcu/detail/MCUBaseInfo.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import {Button} from 'antd';
 import styleObj from './MCUDetail.css';
-import AddMCUInfo from './AddMCUInfo';
+import AddMCUInfo from '../add/AddMCUInfo';
+import {zh_CN_Device} from '@/locale/zh_CN';
+import {en_US_Device} from '@/locale/en_US';
+import {setLocale} from '@/config/i18n';
+import { FormattedMessage, injectIntl } from 'react-intl';
 class MCUBaseInfo extends Component {
     constructor() {
         super();
+        setLocale('zh-CN', zh_CN_Device);
+        setLocale('en-US', en_US_Device);
         this.state = {
             display_name:'block',
             display_edit:'none'
@@ -26,46 +32,46 @@ class MCUBaseInfo extends Component {
     };
     render() {
         return(
-            <div>
-                <div className={styleObj.mcuBaseInfo} style={{display:this.state.display_name }}>
+            <div className={styleObj.mcuBaseInfo}>
+                <div  style={{display:this.state.display_name }}>
                     <div className={styleObj.formItem}>
-                        <div className={styleObj.labelDiv}><span>名称</span> </div>
+                        <div className={styleObj.labelDiv}><FormattedMessage id="MCU_Name"/></div>
                         <div className={styleObj.wrapperDiv}>南京区MCU</div>
                     </div>
                     <div className={styleObj.formItem}>
-                        <div className={styleObj.labelDiv}><span>IP地址</span> </div>
+                        <div className={styleObj.labelDiv}><FormattedMessage id="MCU_AddressIP"/></div>
                         <div className={styleObj.wrapperDiv}>华为</div>
                     </div>
                     <div className={styleObj.formItem}>
-                        <div className={styleObj.labelDiv}><span>区域</span> </div>
+                        <div className={styleObj.labelDiv}><FormattedMessage id="MCU_Zone"/></div>
                         <div className={styleObj.wrapperDiv}>9660</div>
                     </div>
                     <div className={styleObj.formItem}>
-                        <div className={styleObj.labelDiv}><span>对接帐号</span> </div>
+                        <div className={styleObj.labelDiv}><FormattedMessage id="MCU_ConnectAccount"/></div>
                         <div className={styleObj.wrapperDiv}>10.22.22.22</div>
                     </div>
                     <div className={styleObj.formItem}>
-                        <div className={styleObj.labelDiv}><span>对接密码</span> </div>
+                        <div className={styleObj.labelDiv}><FormattedMessage id="MCU_ConnectPassword"/></div>
                         <div className={styleObj.wrapperDiv}>****</div>
                     </div>
                     <div className={styleObj.formItem}>
-                        <div className={styleObj.labelDiv}><span>注册SC地址</span> </div>
+                        <div className={styleObj.labelDiv}><FormattedMessage id="MCU_RegisteSC"/></div>
                         <div className={styleObj.wrapperDiv}>0750</div>
                     </div>
                     <div className={styleObj.formItem}>
-                        <div className={styleObj.labelDiv}><span>MCU标示</span> </div>
+                        <div className={styleObj.labelDiv}><FormattedMessage id="MCU_MCUMark"/> </div>
                         <div className={styleObj.wrapperDiv}>1897644332233</div>
                     </div>
                     <div className={styleObj.formItem}>
-                        <div className={styleObj.labelDiv}><span>管理员帐号</span> </div>
+                        <div className={styleObj.labelDiv}><FormattedMessage id="MCU_AdministratorAccount"/></div>
                         <div className={styleObj.wrapperDiv}>加密</div>
                     </div>
                
                     <div className={styleObj.formItem}>
-                        <div className={styleObj.labelDiv}><span>管理员密码</span> </div>
+                        <div className={styleObj.labelDiv}><FormattedMessage id="MCU_AdministratorPassword"/></div>
                         <div className={styleObj.wrapperDiv}>****</div>
                     </div>
-                    <Button type="primary" className={styleObj.editBtn} onClick={this.toggleEdit}>编辑</Button>
+                    <Button type="primary" className={styleObj.editBtn} onClick={this.toggleEdit}><FormattedMessage id="Edit"/></Button>
                 </div>
                 <div style={{display:this.state.display_edit}}>
                     <AddMCUInfo></AddMCUInfo>
@@ -74,4 +80,4 @@ class MCUBaseInfo extends Component {
         );
     }
 }
-export default MCUBaseInfo;
+export default injectIntl(MCUBaseInfo);
