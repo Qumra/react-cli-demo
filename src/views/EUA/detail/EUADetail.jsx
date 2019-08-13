27fb96b3ecbde7b59aec43020d9c599e80ecd017
Qumra <39854812+Qumra@users.noreply.github.com>
2019-08-13 17:57:49 +0800

    Update and rename src/views/EUA/EUADetail.jsx to src/views/EUA/detail/EUADetail.jsx

--- src/views/EUA/detail/EUADetail.jsx
+++ src/views/EUA/detail/EUADetail.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { Menu, Dropdown, Icon, Tabs } from 'antd';
-import cssObj from './EUADetailFirst.css';
+import cssObj from '../first/EUADetailFirst.css';
 import styleObj from './EUADetail.css';
 import ADBaseInfo from './ADBaseInfo';
 import ServiceAddress from './ServiceAddress';
 import ADSynchronization from './ADSynchronization';
 import ShareNode from './ShareNode';
 import OtherSettings from './OtherSettings';
+import {zh_CN_Device} from '@/locale/zh_CN';
+import {en_US_Device} from '@/locale/en_US';
+import {setLocale} from '@/config/i18n';
+import { FormattedMessage, injectIntl } from 'react-intl';
 const {TabPane} = Tabs;
 const menu = (
     <Menu>
@@ -16,23 +20,35 @@ const menu = (
         </Menu.Item>
         <Menu.Item>
             <Icon type="setting" theme="outlined" />
-        手动全量同步
+            手动全量同步
+        </Menu.Item>
+        <Menu.Item>
+            <Icon type="setting" theme="outlined" />
+            重置SMC访问密码
+        </Menu.Item>
+        <Menu.Item>
+            <Icon type="setting" theme="outlined" />
+            重置TMS访问密码
         </Menu.Item>
     </Menu>
 );
 class EUADetail extends Component {
+    constructor() {
+        super();
+        setLocale('zh-CN', zh_CN_Device);
+        setLocale('en-US', en_US_Device);
+    }
     render() {
-        
+        const { intl } = this.props;
         return (<div className={cssObj.euaMain}>
-            <div className={cssObj.leftNav}>111</div>
             <div className={cssObj.euaContent}>
                 <div className={cssObj.euaContentTitle}>
-                    <span>企业通讯录管理</span>
+                    <FormattedMessage id="EUA_title"/>
                     <div className={styleObj.euaContentTitleMid}>
                         <div className={styleObj.midContent}>
                             <div className={styleObj.status}>
                                 <div className={styleObj.greenCircle}></div>
-                                <span>已连接</span>
+                                <FormattedMessage id="EUA_Connected"/>
                             </div>
                             <div className={styleObj.mainIp}>
                                 <div className={styleObj.mainIpIcon}></div>
@@ -44,18 +60,23 @@ class EUADetail extends Component {
                         </div>
                     </div>
                     <div className={styleObj.euaContentTitleRight}>
-                        <div className={styleObj.smcPassword}>
+                        {/* <div className={styleObj.smcPassword}>
                             <div className={styleObj.smcPasswordIcon}></div>
                             <span className={styleObj.smcPasswordText}>重置SMC访问密码</span>
                         </div>
                         <div className={styleObj.tmsPassword}>
                             <div className={styleObj.smcPasswordIcon}></div>
                             <span className={styleObj.smcPasswordText}>重置TMS访问密码</span>
+                        </div> */}
+                        <div className={styleObj.manualSync}>
+                            <div className={styleObj.manualSyncIcon}></div>
+                            <FormattedMessage id="EUA_ManualGlobal" className={styleObj.manualSyncText}/>
                         </div>
                         <div className={styleObj.more}>
                             <Dropdown overlay={menu}>
                                 <div>
-                                    <Icon type="ellipsis" theme="outlined" />更多
+                                    <Icon type="ellipsis" theme="outlined" />
+                                    <FormattedMessage id="More"/>
                                 </div> 
                             </Dropdown>
                         </div>
@@ -67,23 +88,23 @@ class EUADetail extends Component {
                 <div className={cssObj.euaContentMid}>
                     <div className={cssObj.firstLink}>
                         <Tabs defaultActiveKey="1">
-                            <TabPane tab="基本信息" key="1">
+                            <TabPane tab={intl.formatMessage({id: 'BasicInfo'})} key="1">
                                 <ADBaseInfo></ADBaseInfo>
                             </TabPane>
-                            <TabPane tab="业务地址" key="2">
+                            <TabPane tab={intl.formatMessage({id: 'ServiceAddress'})} key="2">
                                 <ServiceAddress></ServiceAddress>
                             </TabPane>
-                            <TabPane tab="AD同步配置" key="3">
+                            <TabPane tab={intl.formatMessage({id: 'ADSynchronization'})} key="3">
                                 <ADSynchronization></ADSynchronization>
                             </TabPane>
-                            <TabPane tab="共享组织" key="4">
+                            <TabPane tab={intl.formatMessage({id: 'SharedNode'})} key="4">
                                 <ShareNode></ShareNode>
                             </TabPane>
-                            <TabPane tab="其他配置" key="5">
+                            <TabPane tab={intl.formatMessage({id: 'OtherSettings'})} key="5">
                                 <OtherSettings></OtherSettings>
                             </TabPane>
-                            <TabPane tab="日志" key="6">Content of Tab Pane 3</TabPane>
-                            <TabPane tab="告警" key="7">Content of Tab Pane 3</TabPane>
+                            <TabPane tab={intl.formatMessage({id: 'Log'})} key="6">Content of Tab Pane 3</TabPane>
+                            <TabPane tab={intl.formatMessage({id: 'Alarm'})} key="7">Content of Tab Pane 3</TabPane>
                         </Tabs>
                     </div>
                 </div>
@@ -91,4 +112,4 @@ class EUADetail extends Component {
         </div>);
     }
 }
-export default EUADetail;
+export default injectIntl(EUADetail);
